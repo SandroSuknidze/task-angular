7317ea98574700d3c66173f29502c99556f80d24
sandrors <sandro.suknidze.1@btu.edu.ge>
2024-10-09 10:36:24 +0400

    + pdf download/upload

--- src/app/pages/housing-location-form/housing-location-form.component.ts
+++ src/app/pages/housing-location-form/housing-location-form.component.ts
@@ -3,6 +3,7 @@ import { HousingLocation } from '../../services/product/housing-location';
 import { ProductService } from '../../services/product/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { read } from '@popperjs/core';
 
 @Component({
   selector: 'app-housing-location-form',
@@ -16,6 +17,7 @@ export class HousingLocationFormComponent implements OnInit {
   fb = inject(NonNullableFormBuilder);
   imageError: string | null = null;
   imagePreview: string | null = null;
+  pdfError: string | null = null;
 
   housingLocationForm = this.fb.group({
     name: ['', { validators: [Validators.required] }],
@@ -25,6 +27,7 @@ export class HousingLocationFormComponent implements OnInit {
     availableUnits: [0, [Validators.required, Validators.min(0)]],
     wifi: [false],
     laundry: [false],
+    pdf: ['', Validators.required]
   });
 
   constructor(
@@ -75,6 +78,53 @@ export class HousingLocationFormComponent implements OnInit {
     }
   }
 
+  onPdfSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    console.log(file);
+    if(file) {
+      const validTypes = ['application/pdf'];
+      if(!validTypes.includes(file.type)) {
+        this.pdfError = 'Only PDF is allowed.';
+        return;
+      }
+      this.pdfError = null;
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.housingLocationForm.patchValue({ pdf: e.target?.result as string})
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  downloadPdf() {
+    if(this.housingLocationId) {
+      const pdfData = this.housingLocationForm.value.pdf; 
+
+      if (pdfData) {
+        const byteCharacters = atob(pdfData); // Decode base64
+        const byteNumbers = new Array(byteCharacters.length);
+        
+        for (let i = 0; i < byteCharacters.length; i++) {
+          byteNumbers[i] = byteCharacters.charCodeAt(i);
+        }
+    
+        const byteArray = new Uint8Array(byteNumbers);
+        const blob = new Blob([byteArray], { type: 'application/pdf' });
+    
+        // Create a temporary anchor element for download
+        const link = document.createElement('a');
+        const url = window.URL.createObjectURL(blob);
+        link.href = url;
+        link.download = 'document.pdf'; // Specify the filename
+        link.click();
+    
+        // Clean up after download
+        window.URL.revokeObjectURL(url);
+      }
+    }
+  }
+
   onSubmit() {
     if (this.housingLocationForm.valid) {
       const cardData: HousingLocation = {
@@ -86,6 +136,7 @@ export class HousingLocationFormComponent implements OnInit {
         availableUnits: this.housingLocationForm.value.availableUnits!,
         wifi: this.housingLocationForm.value.wifi!,
         laundry: this.housingLocationForm.value.laundry!,
+        pdf: this.housingLocationForm.value.pdf!,
       };
 
       if (this.isEditMode) {
